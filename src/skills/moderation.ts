5d716e8fd632c0a67e96dd3d9828f31857f0c972
sheverniskiy <hwork@list.ru>
2022-05-17 20:39:25 +0300

    fix init sequence

--- src/skills/moderation.ts
+++ src/skills/moderation.ts
@@ -23,13 +23,8 @@ export default class Moderation {
   }
 
   async init(): Promise<void> {
-    this.loadAdmins(this._chatId, this._config.admins).then((result) => {
-      this._admins = result
-    })
-
-    this.loadPermissions(this._chatId).then((result) => {
-      this._permissions = result
-    })
+    this._admins = await this.loadAdmins(this._chatId, this._config.admins)
+    this._permissions = await this.loadPermissions(this._chatId)
 
     this.setCommands(this._token, this._chatId, 'chat_administrators', this._config.commands.admin)
     this.setCommands(this._token, this._chatId, 'chat', this._config.commands.user)
